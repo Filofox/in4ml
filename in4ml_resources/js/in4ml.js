9f05b466db27f6475b48f646e014dd9336288ac2
man.stick <man.stick@198d28e0-b347-861d-5d0c-802d3c66bb7e>
2010-07-19 16:23:11 +0000

    Fixed relative URL problem with TinyMCE

--- in4ml_resources/js/in4ml.js
+++ in4ml_resources/js/in4ml.js
@@ -1392,7 +1392,8 @@ JSLibInterface_jQuery.prototype.ConvertToRichText = function( element, options )
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "left",
 		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : true
+		theme_advanced_resizing : true,
+		relative_urls: false
 	};
 
 	var settings = $.extend(true, defaults, options)
