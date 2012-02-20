f1b146b3750bc3c2aa12a6a0e6e39b8eb25caf65
man.stick <man.stick@198d28e0-b347-861d-5d0c-802d3c66bb7e>
2012-02-20 13:31:49 +0000

    Added container class support to InlineLabel

--- in4ml/elements/blocks/in4mlBlockInlineLabel.class.php
+++ in4ml/elements/blocks/in4mlBlockInlineLabel.class.php
@@ -12,16 +12,17 @@ require_once( in4ml::GetPathBlocks() . 'in4mlBlockContainer.class.php' );
  */
 class in4mlBlockInlineLabel extends in4mlBlock{
 	public $type = 'InlineLabel';
-	
+
 	public $label;
 	public $field_name;
 	public $prefix;
 	public $suffix;
 	public $notes;
+	public $errors;
 
 	public function __construct(){
 		parent::__construct();
-		
+
 		$this->AddClass( 'container' );
 	}
 
@@ -34,11 +35,33 @@ class in4mlBlockInlineLabel extends in4mlBlock{
 	 */
 	public function GetRenderValues(){
 		$values = parent::GetRenderValues();
-		
+
+		// Check for element container classes
+		foreach( $this->elements as $element ){
+			// Check for validator classes
+			if( is_subclass_of( $element, 'in4mlField' ) ){
+				foreach( $element->GetValidators() as $validator ){
+					if( $validator_class = $validator->GetClass( $element ) ){
+						$this->AddClass( strtolower( $validator_class ) );
+					}
+				}
+				if( $element->container_id ){
+					$this->id = $element->container_id;
+				}
+			}
+
+			foreach( $element->GetContainerClasses() as $class ){
+				$this->AddClass( $class );
+			}
+
+		}
+		$values = parent::GetRenderValues();
+
 		$values->prefix = $this->prefix;
 		$values->suffix = $this->suffix;
 		$values->notes = $this->notes;
-		
+		$values->errors = $this->errors;
+
 		return $values;
 	}
 }
