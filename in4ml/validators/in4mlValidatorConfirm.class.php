cadd32356875ac8a92554d16e87511edabe6ca7f
man.stick <man.stick@198d28e0-b347-861d-5d0c-802d3c66bb7e>
2012-07-02 16:55:21 +0000

    Set notes to null, not empty string, in Confirm validator

--- in4ml/validators/in4mlValidatorConfirm.class.php
+++ in4ml/validators/in4mlValidatorConfirm.class.php
@@ -11,30 +11,30 @@ require_once( in4ml::GetPathCore() . 'in4mlValidator.class.php' );
  * Check that confirm field value matches original field value
  */
 class in4mlValidatorConfirm extends in4mlValidator{
-	
+
 	protected $class = 'is_confirm';
-	
+
 	/**
 	 * Perform validation
-	 * 
+	 *
 	 * @param		in4mlField		$field		The field to be validated
 	 *
 	 * @return		boolean						False if the field is not valid
 	 */
 	public function ValidateField( in4mlField $field ){
-		
+
 		$output = true;
-		
+
 		if( $field->confirm_field ){
 			if( $field->GetValue() != $field->confirm_field->GetValue() ){
 				$field->SetError( $this->GetErrorText( "confirm" ) );
 				$output = false;
 			}
 		}
-		
+
 		return $output;
 	}
-	
+
 	/**
 	 * Return appropriate class according to whether this is the confirm field or the original field
 	 *
@@ -48,10 +48,10 @@ class in4mlValidatorConfirm extends in4mlValidator{
 		}
 		return $this->class;
 	}
-	
+
 	/**
 	 * Modify field -- adds a clone 'confirm' field
-	 * 
+	 *
 	 * @param		in4mlField		$field
 	 *
 	 * @return		array
@@ -60,10 +60,12 @@ class in4mlValidatorConfirm extends in4mlValidator{
 		$confirm_field = clone $element;
 		$confirm_field->name .= '_confirm';
 		$confirm_field->label = 'Confirm ' . $confirm_field->label;
-		$confirm_field->notes = '';
+		if( isset( $confirm_field->notes ) ){
+			$confirm_field->notes = null;
+		}
 
 		$element->confirm_field = $confirm_field;
-		
+
 		return array
 		(
 			$element,
